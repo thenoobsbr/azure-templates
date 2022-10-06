015b68dae0a65a1a54dd8969711f8ed6a840de95
Tiago Resende <tiagor87@gmail.com>
2022-10-06 18:58:31 -0300

    :tada: Create project

---
+++ scripts/calculate_version.sh
@@ -0,0 +1,147 @@
+echo "======================================="
+echo "              INPUT                    "
+echo "======================================="
+echo "VersionMessage: $SOURCE_VERSION_MESSAGE"
+echo "Branch: $SOURCE_BRANCH"
+if [[ ! -z "$PR_SOURCE_BRANCH" ]] &&
+    ! [[ $PR_SOURCE_BRANCH =~ "PullRequest" ]];
+then
+    echo "PR Branch: $PR_SOURCE_BRANCH"
+    export SOURCE_BRANCH="$PR_SOURCE_BRANCH";
+fi
+echo "CurrentCounter: $RELEASE"
+echo "Reason: $REASON"
+echo "======================================="
+
+if [[ $SOURCE_BRANCH =~ "/tags/" ]];
+then
+    export VERSION="$(echo $SOURCE_BRANCH | cut -d'/' -f3)"
+    
+    echo "========================="
+    echo "      TAG $VERSION       "
+    echo "========================="
+    
+    echo "##vso[task.setvariable variable=version;isOutput=true]$VERSION"
+    echo "##vso[task.setvariable variable=staging_deploy;isOutput=true]True"
+    echo "##vso[task.setvariable variable=production_deploy;isOutput=true]True"
+    echo "##vso[build.updatebuildnumber]$VERSION"
+    
+    echo "======================================="
+    echo "              OUTPUT                   "
+    echo "======================================="
+    echo "Version: $VERSION"
+    echo "Staging deploy: TRUE"
+    echo "Production deploy: TRUE"
+    echo "======================================="
+    
+    exit 0
+fi
+
+export SOURCE_BRANCH="/$(echo $SOURCE_BRANCH | cut -d'/' -f3)/$(echo $SOURCE_BRANCH | cut -d'/' -f4)"
+
+if [[ $SOURCE_BRANCH =~ "/feature/" ]];
+then
+    echo "========================="
+    echo "         FEATURE         "
+    echo "========================="
+    
+    export VERSION_PREFIX="$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH"
+    export VERSION_SUFFIX="beta.${RELEASE}"
+    export VERSION="$VERSION_PREFIX-$VERSION_SUFFIX"
+    
+    echo "##vso[task.setvariable variable=version;isOutput=true]$VERSION"
+    echo "##vso[task.setvariable variable=staging_deploy;isOutput=true]False"
+    echo "##vso[task.setvariable variable=production_deploy;isOutput=true]False"
+    echo "##vso[build.updatebuildnumber]$VERSION"
+    
+    echo "======================================="
+    echo "              OUTPUT                   "
+    echo "======================================="
+    echo "Version: $VERSION"
+    echo "Staging deploy: FALSE"
+    echo "Production deploy: FALSE"
+    echo "======================================="
+    
+    exit 0
+fi
+
+if [[ $SOURCE_BRANCH =~ "/bugfix/" ]];
+then
+    echo "========================="
+    echo "         BUGFIX          "
+    echo "========================="
+    
+    export VERSION_PREFIX="$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH"
+    export VERSION_SUFFIX="beta.${RELEASE}"
+    export VERSION="$VERSION_PREFIX-$VERSION_SUFFIX"
+    
+    echo "##vso[task.setvariable variable=version;isOutput=true]$VERSION"
+    echo "##vso[task.setvariable variable=staging_deploy;isOutput=true]False"
+    echo "##vso[task.setvariable variable=production_deploy;isOutput=true]False"
+    echo "##vso[build.updatebuildnumber]$VERSION"
+    
+    echo "======================================="
+    echo "              OUTPUT                   "
+    echo "======================================="
+    echo "Version: $VERSION"
+    echo "Staging deploy: FALSE"
+    echo "Production deploy: FALSE"
+    echo "======================================="
+    
+    exit 0
+fi
+
+if [[ $SOURCE_BRANCH =~ "/hotfix/" ]] ||
+    [[ $SOURCE_BRANCH =~ "/release/" ]];
+then
+    echo "========================="
+    echo "    HOTFIX / RELEASE     "
+    echo "========================="
+    
+    export VERSION="$(echo $SOURCE_BRANCH | cut -d'/' -f3)"
+    export VERSION_MAJOR="$(echo $VERSION | cut -d'.' -f1)"
+    export VERSION_MINOR="$(echo $VERSION | cut -d'.' -f2)"
+    export VERSION_PATCH="$(echo $VERSION | cut -d'.' -f3)"
+    
+    export VERSION_PREFIX="$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH"
+    export VERSION_SUFFIX="rc.${RELEASE}"
+    export VERSION="$VERSION_PREFIX-$VERSION_SUFFIX"
+    
+    echo "##vso[task.setvariable variable=version;isOutput=true]$VERSION"
+    echo "##vso[task.setvariable variable=staging_deploy;isOutput=true]True"
+    echo "##vso[task.setvariable variable=production_deploy;isOutput=true]False"
+    echo "##vso[build.updatebuildnumber]$VERSION"
+    
+    echo "======================================="
+    echo "              OUTPUT                   "
+    echo "======================================="
+    echo "Version: $VERSION"
+    echo "Staging deploy: TRUE"
+    echo "Production deploy: FALSE"
+    echo "======================================="
+    
+    exit 0
+fi
+
+echo "======================================="
+echo "              OTHERS                   "
+echo "======================================="
+
+export VERSION_PREFIX="$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH"
+export VERSION_SUFFIX="alpha.${RELEASE}"
+export VERSION="$VERSION_PREFIX-$VERSION_SUFFIX"
+
+echo "##vso[task.setvariable variable=version;isOutput=true]$VERSION"
+echo "##vso[task.setvariable variable=staging_deploy;isOutput=true]False"
+echo "##vso[task.setvariable variable=production_deploy;isOutput=true]False"
+echo "##vso[build.updatebuildnumber]$VERSION"
+
+echo "======================================="
+echo "              OUTPUT                   "
+echo "======================================="
+echo "Version: $VERSION"
+echo "Staging deploy: FALSE"
+echo "Production deploy: FALSE"
+echo "======================================="
+
+exit 0
