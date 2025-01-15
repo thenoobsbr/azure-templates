09197aee8cabed86f17978bd8ca0149b3f79b930
Tiago Resende <tiagor87@gmail.com>
2025-01-15 15:47:50 -0300

    :construction: fix

--- scripts/calculate_manual_version.sh
+++ scripts/calculate_manual_version.sh
@@ -10,10 +10,10 @@ echo "Suffix: $SUFFIX"
 echo "#######################################"
 
 echo "#######################################"
-echo "#            LAST VERSION             #"
+echo "#            CALCULATING              #"
 echo "#######################################"
 
-if [[ $PROJECT_PATH =~ ".csproj" ]]; then
+if [[ $PROJECT_PATH == *.csproj ]]; then
   echo "The Project is a .csproj"
   echo "Reading last version from .csproj"
   
@@ -21,24 +21,23 @@ if [[ $PROJECT_PATH =~ ".csproj" ]]; then
   if [[ -z $last_version ]]; then
     last_version="0.0.0"
   fi
+
+  major="$(echo $last_version | cut -d'.' -f1)"
+  minor="$(echo $last_version | cut -d'.' -f2)"
+  patch="$(echo $last_version | cut -d'.' -f3)"
 else
   cd $PROJECT_PATH
   git fetch --tags --prune
   last_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
+  
+  if [[ -z $last_tag ]]; then
+    last_tag="0.0.0"
+  fi
+
   major="$(echo $last_tag | cut -d'.' -f1)"
   minor="$(echo $last_tag | cut -d'.' -f2)"
   patch="$(echo $last_tag | cut -d'.' -f3)"
-end
-
-echo "Last version: $last_tag"
-echo "Major: $major"
-echo "Minor: $minor"
-echo "Patch: $patch"
-echo "#######################################"
-
-echo "#######################################"
-echo "#           NEW VERSION               #"
-echo "#######################################"
+fi
 
 if [[ $INCREMENT == "major" ]]; then
   major=$((major+1))
@@ -57,10 +56,14 @@ if [[ $SUFFIX != "" ]]; then
   new_version="$new_version-$SUFFIX.$RELEASE"
 fi
 
+echo "Last version: $last_tag"
 echo "New version: $new_version"
 echo "Major: $major"
 echo "Minor: $minor"
 echo "Patch: $patch"
 echo "#######################################"
 
-echo "##vso[task.setvariable variable=version;isOutput=true]$new_version"
+echo "##vso[task.setvariable variable=last_version;isOutput=true]$last_version"
+echo "##vso[task.setvariable variable=new_version;isOutput=true]$new_version"
+
+exit 0
