59c7d58cc6060576859869ed7204da7ccf449b59
Tiago Resende <tiagor87@gmail.com>
2025-01-16 19:20:40 -0300

    :construction: get version

--- azure/scripts/get_version_by_dll.sh
+++ azure/scripts/get_version_by_dll.sh
@@ -16,7 +16,7 @@ echo "#####################"
 echo "#    Get version    #"
 echo "#####################"
 
-version=$(exiftool ${DLL_PATH} | grep -i Version)
+version=$(exiftool ${DLL_PATH} | grep -i "Production Version")
 
 echo "Version: $version"
 echo "#####################"
