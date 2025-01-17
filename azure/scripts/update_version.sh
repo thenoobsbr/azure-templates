d8f0d44375ccf8576710e8913875721280a21ae3
Tiago Resende <tiagor87@gmail.com>
2025-01-16 21:12:13 -0300

    :construction: update version in block

--- azure/scripts/update_version.sh
+++ azure/scripts/update_version.sh
@@ -5,15 +5,15 @@ echo "#       Input       #"
 echo "#####################"
 echo "UserEmail: $USER_EMAIL"
 echo "UserName: $USER_NAME"
+echo "ArtifactPath: $ARTIFACT_PATH"
 echo "SourcePath: $SOURCE_PATH"
 echo "SourceBranch: $SOURCE_BRANCH"
-echo "ProjectPath: $PROJECT_PATH"
-echo "Version: $VERSION"
 echo "#####################"
 
 echo "#####################"
-echo "#    Install tool   #"
+echo "#   Install tools   #"
 echo "#####################"
+sudo apt install -y exiftool
 dotnet tool install -g dotnet-setversion
 
 echo "#####################"
@@ -22,7 +22,31 @@ echo "#####################"
 echo "#  Change version   #"
 echo "#####################"
 
-setversion $VERSION $PROJECT_PATH
+for folder in "$ARTIFACT_PATH"/*; do
+  if [ ! -d "$folder" ]; then
+    continue
+  fi
+  
+  echo "Found artifact: $folder"
+
+  dll_path=$(find "$folder" -type f -name "$folder.dll" | head -n 1)
+  if [ -z "$dll_path" ]; then
+    continue
+  fi
+  echo "Found DLL: $dll_path"
+
+  version=$(exiftool $dll_path | grep "Product Version  " | awk -F ': ' '{print $2}')
+  echo "Version: $version"
+
+  project_path=$(find "$SOURCE_FOLDER" -type f -name "$folder.csproj" | head -n 1)
+  if [ -z "$project_path" ]; then
+    continue
+  fi
+  echo "Found project: $project_path"
+
+  setversion $version $project_path
+  echo "Updated project: $project_path with version: $version"
+done
 
 echo "#####################"
 
@@ -34,7 +58,7 @@ cd $SOURCE_PATH
 
 git config --global user.email "${USER_EMAIL}"
 git config --global user.name "${USER_NAME}"
-git add .
+git add . --all
 git commit -m ":bookmark: update version"
 git push origin HEAD:$SOURCE_BRANCH
 
