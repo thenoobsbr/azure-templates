c006551dc66ad94b9f5aa634d2bf123faa35adb4
Tiago Resende <tiagor87@gmail.com>
2025-02-03 20:36:25 -0300

    :bug: fix version calculation

--- azure/scripts/get_version_by_dll.sh
+++ azure/scripts/get_version_by_dll.sh
@@ -16,16 +16,31 @@ echo "#####################"
 echo "#    Get version    #"
 echo "#####################"
 
-version=$(exiftool ${DLL_PATH} | grep "Product Version  " | awk -F ': ' '{print $2}')
+version=$(exiftool "${DLL_PATH}" | grep "Product Version  " | awk -F ': ' '{print $2}')
 
-major="$(echo $version | cut -d'.' -f1)"
-minor="$(echo $version | cut -d'.' -f2)"
-patch="$(echo $version | cut -d'.' -f3)"
-suffix="${patch*-%}"
-patch="${patch%-*}"
-revision="$(echo $version | cut -d'.' -f4)"
+# Extrai componentes principais
+major=$(echo "$version" | cut -d'.' -f1)
+minor=$(echo "$version" | cut -d'.' -f2)
+patch_part=$(echo "$version" | cut -d'.' -f3)
 
-version_migration="$major.$minor.$patch-${suffix}-migration.${revision}"
+# Separa patch e sufixo
+patch="${patch_part%%-*}"         # Remove tudo após o primeiro hífen
+suffix="${patch_part#$patch}"     # Pega o sufixo (se existir)
+suffix="${suffix#-}"              # Remove o hífen inicial do sufixo
+
+# Extrai revisão (se existir)
+revision=$(echo "$version" | cut -d'.' -f4)
+
+# Monta a versão de migração
+if [ -n "$suffix" ]; then
+    # Caso COM sufixo
+    version_migration="$major.$minor.$patch-$suffix-migration"
+    [ -n "$revision" ] && version_migration="$version_migration.$revision"
+else
+    # Caso SEM sufixo
+    version_migration="$major.$minor.$patch-migration"
+    [ -n "$revision" ] && version_migration="$version_migration.$revision"
+fi
 
 echo "Major: $major"
 echo "Minor: $minor"
